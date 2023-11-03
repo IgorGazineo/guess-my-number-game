9b7bc8817c7d6387d5ed44f9a96434f328b646ca
Igor Gazineo <gaumgazineo@hotmail.com>
2023-11-03 11:08:04 -0300

    iniciando o Javascript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const checkBTN = document.querySelector('.check');
+const againBTN = document.querySelector('.again');
+let number = Math.trunc(Math.random() * 20) + 1;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+checkBTN.addEventListener('click', () => {
+  let guess = +document.querySelector('.guess').value;
+  console.log(number, guess);
+  if (number !== guess) {
+    displayMessage(guess > number ? 'Too high' : 'Too low');
+  }
+});
